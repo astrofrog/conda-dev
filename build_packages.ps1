e41e10e1c5af172ad533a344033212bdf9ec9c08
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-27 12:29:54 +0100

    Fix powershell

--- build_packages.ps1
+++ build_packages.ps1
@@ -7,10 +7,10 @@ conda config --set anaconda_upload no
 
 cd recipes
 
-$CONDA_BUILD = "conda build --python $env:PYTHON_VERSION"
+conda build --python $env:PYTHON_VERSION glue-core
+$BUILD_OUTPUT = cmd /c conda build --python $env:PYTHON_VERSION glue-core --output 2>&1
+echo $BUILD_OUTPUT
 
-$CONDA_BUILD glue-core
-$CONDA_BUILD glue-core --output
 if ($env:TRAVIS_EVENT_TYPE -eq "push" -and $env:TRAVIS_BRANCH -eq "appveyor") {
-  anaconda -t $env:ANACONDA_TOKEN upload --force -l dev "$($CONDA_BUILD glue-core --output)";
+  anaconda -t $env:ANACONDA_TOKEN upload --force -l dev $BUILD_OUTPUT;
 }
