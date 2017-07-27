e51279ddf00a130bda83bf44f183bb28be1abc35
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-27 12:51:56 +0100

    Fix syntax

--- build_packages.ps1
+++ build_packages.ps1
@@ -6,7 +6,7 @@ activate root
 
 conda install -n root conda-build=2 anaconda-client
 
-# Don't auto-upload, instead we upload manually specifying a token
+# Don't auto-upload, instead we upload manually specifying a token.
 conda config --set anaconda_upload no
 
 cd recipes
@@ -15,6 +15,6 @@ conda build --python $env:PYTHON_VERSION glue-core
 $BUILD_OUTPUT = cmd /c conda build --python $env:PYTHON_VERSION glue-core --output 2>&1
 echo $BUILD_OUTPUT
 
-if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq "" -and $env:APPVEYOR_REPO_BRANCH -eq "appveyor") {
+if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null -and $env:APPVEYOR_REPO_BRANCH -eq "appveyor") {
   anaconda -t $env:ANACONDA_TOKEN upload --force -l dev $BUILD_OUTPUT;
 }
